09b91e09ddcf953b6e1ea4676e4796bfd20221e2
hcl-1024 <lhanchin59@gmail.com>
2025-01-16 17:03:01 +0800

    first commit

--- myproject/app.py
+++ myproject/app.py
@@ -1,40 +1,35 @@
-from flask import Flask, g, render_template, request, redirect, url_for
-import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-DATABASE = 'people.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
 
 @app.route('/')
 def index():
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM people")
-    people = cursor.fetchall()
-    cursor.close()
+    people = Person.query.all()
     return render_template('index.html', people=people)
 
 @app.route('/add', methods=['POST'])
 def add():
-    db = get_db()
-    cursor = db.cursor()
     name = request.form['name']
-    cursor.execute("INSERT INTO people (name) VALUES (?)", (name,))
-    db.commit()
-    cursor.close()
-    return redirect(url_for('index'))
+    # Check if the name already exists in the database
+    existing_person = Person.query.filter_by(name=name).first()
+
+    if existing_person:
+        return "Name already exists in the database!"
+    else:
+        new_person = Person(name=name)
+        db.session.add(new_person)
+        db.session.commit()
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
