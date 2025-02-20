a20adfe09bffdb14a1b5289ab48aaac0bf6ad676
hcl-1024 <lhanchin59@gmail.com>
2025-02-20 17:15:17 +0800

    baseurl

--- myproject/app.py
+++ myproject/app.py
@@ -3,10 +3,8 @@ import sqlite3
 import datetime
 
 
-#baseurl = "/smartcities"
-#app = Flask(__name__, static_url_path=baseurl)
-app = Flask(__name__)
-
+baseurl = "/smartcities"
+app = Flask(__name__, static_url_path=baseurl)
 
 
 # Function to connect to the database
@@ -30,7 +28,7 @@ def init_db():
     conn.commit()
     conn.close()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route(baseurl + '/', methods=['GET', 'POST'])
 def index():
     init_db()
     if request.method == 'POST':
@@ -58,7 +56,7 @@ def index():
     
     return render_template('index.html', entries=entries[:10])
 
-@app.route('/finish', methods=['GET', 'POST'])
+@app.route(baseurl + '/finish', methods=['GET', 'POST'])
 def finish():
     init_db()
     now = datetime.datetime.now()
